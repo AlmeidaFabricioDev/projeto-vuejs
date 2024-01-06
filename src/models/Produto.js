ab374b2060ecdb6003f91d206ec30c7a5094bfda
AlmeidaFabricioDev <devfabsalmeida@gmail.com>
2024-01-06 18:00:45 -0400

    Inclusao backend em node e serivces da api produtos

---
+++ src/models/Produto.js
@@ -0,0 +1,11 @@
+export default class produto {
+    constructor(obj) {
+      obj = obj || {};
+      this.id = obj.id;
+      this.nome = obj.nome;
+      this.valor = obj.valor || 0;
+      this.quantidadeEstoque = obj.quantidadeEstoque || 0;
+      this.dataCadastro = obj.dataCadastro;
+    }
+  }
+  
